164829a186dad45e4734828951d52d24319802a1
Henry <henry232323@users.noreply.github.com>
2020-03-20 12:42:37 -0500

    Update convexhull.py

--- pylygon/convexhull.py
+++ pylygon/convexhull.py
@@ -12,7 +12,7 @@
 
 
 from numpy import array, in1d, lexsort
-
+from functools import reduce
 
 
 
@@ -25,12 +25,15 @@ def _turn(i, j, k):
     global P
     _P = P
     (p_x, p_y), (q_x, q_y), (r_x, r_y) = _P[i], _P[j], _P[k]
-    return cmp((q_x - p_x) * (r_y - p_y) - (r_x - p_x) * (q_y - p_y), 0)
+    a = (q_x - p_x) * (r_y - p_y) - (r_x - p_x) * (q_y - p_y)
+    return (a>0)-(a<0)
 
 
 def _keep_left(hull, r):
-    while len(hull) > 1 and _turn(hull[-2], hull[-1], r) != TURN_LEFT: hull.pop()
-    if not len(hull) or not (hull[-1] == r).all(): hull.append(r)
+    while len(hull) > 1 and _turn(hull[-2], hull[-1], r) != TURN_LEFT:
+        hull.pop()
+    if not len(hull) or not (hull[-1] == r).all():
+        hull.append(r)
     return hull
 
 
@@ -45,5 +48,5 @@ def convexhull(_P):
     I = lexsort((_P[:,1],_P[:,0]))
     l = reduce(_keep_left, I, [])
     u = reduce(_keep_left, reversed(I), [])
-    l.extend(u[i] for i in xrange(1, len(u) - 1))
+    l.extend(u[i] for i in range(1, len(u) - 1))
     return array(l)
